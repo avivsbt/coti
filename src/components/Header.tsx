b1587934a12d1ae13c9947b8dbd430c78ac3deb6
avivmi <avivmi@essencesecurity.com>
2022-05-19 17:05:31 +0300

    logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,9 @@ import React from 'react'
 export default function Header() {
   return (
     <div className='header'>
-        <h1 className='logo'>Coti</h1>
+        <h1 className='logo'>
+          <img src="https://coti.io/static/media/coti_logo.cf990b20.svg" alt="Coti"/>
+        </h1>
     </div>
   )
 }
