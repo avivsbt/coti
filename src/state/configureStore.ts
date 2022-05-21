e44e3652a905dd20f1c8269e512c71660dc4fc12
avivmi <avivmi@essencesecurity.com>
2022-05-21 19:46:31 +0300

    toolkit - first commit

--- src/state/configureStore.ts
+++ src/state/configureStore.ts
@@ -1,20 +1,32 @@
-import { combineReducers, createStore, applyMiddleware } from "redux";
+import { configureStore, combineReducers, getDefaultMiddleware } from "@reduxjs/toolkit";
+
+
+//import { combineReducers, createStore, applyMiddleware } from "redux";
 import createSagaMiddleware from "redux-saga";
 import countriesReducer from "./reducers/countries";
-import { composeWithDevTools } from 'redux-devtools-extension'
+// import { composeWithDevTools } from 'redux-devtools-extension'
 import { watcherSaga } from "./sagas/rootSaga";
 
-const reducers = combineReducers({
+const reducer = combineReducers({
     countries: countriesReducer
 });
 
 const sagaMiddleware = createSagaMiddleware();
-const middleware = [sagaMiddleware]; 
-const enhancers = [applyMiddleware(...middleware)]
-const composedEnhancers = composeWithDevTools(...enhancers)
 
-const store = createStore(reducers, {}, composedEnhancers); 
+const store = configureStore({
+    reducer,
+    middleware:[...getDefaultMiddleware({thunk: false}), sagaMiddleware],
+    devTools: process.env.NODE_ENV !== 'production'
+});
 sagaMiddleware.run(watcherSaga);
 
+// const middleware = [sagaMiddleware]; 
+// const enhancers = [applyMiddleware(...middleware)]
+// const composedEnhancers = composeWithDevTools(...enhancers)
+
+// const store = createStore(reducers, {}, composedEnhancers);  
+// sagaMiddleware.run(watcherSaga);
+
 export default store;
-export type RootState = ReturnType<typeof reducers>
+
+export type RootState = ReturnType<typeof reducer>
