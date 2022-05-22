843288ad9c2f8a234a03cbb582eced768c117f20
avivmi <avivmi@essencesecurity.com>
2022-05-22 07:53:21 +0300

    remove ActionType

--- src/state/action-types/countries.ts
+++
@@ -1,6 +0,0 @@
-export enum ActionType {
-    GET_COUNTRIES = "get_countries",
-    SET_COUNTRIES = "set_countries",
-    SET_FILTER_COUNTRIES = "set_filter_countries",
-    SET_CURRENT_COUNTRY = "set_current_country"
-}
